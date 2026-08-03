74bf708934ba0fc099658a7744f0f64fff32ecc9
17671052998-art <17671052998@163.com>
2026-08-03 19:06:14 +0800

    Configure GitHub Pages base path

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base:
+    process.env.GITHUB_PAGES === "true" ? "/pet-admin-prototype/" : "/",
   build: {
     outDir: "dist/client",
   },
